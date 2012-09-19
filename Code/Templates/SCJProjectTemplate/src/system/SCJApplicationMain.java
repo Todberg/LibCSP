f3cfbff55477cd9ebb4393b0d4682151d79e158b
Jeppe Andersen <nocture@nocture.dk>
2012-09-19 16:06:56 +0200

    Eclipse project

---
+++ Code/Templates/SCJProjectTemplate/src/system/SCJApplicationMain.java
@@ -0,0 +1,12 @@
+package system;
+
+import javax.safetycritical.JopSystem;
+
+public class SCJApplicationMain
+{
+	public static void main(String[] args)
+	{
+		// Start the safelet from here
+		//JopSystem.startMission(new Safelet()); 
+	}
+}
