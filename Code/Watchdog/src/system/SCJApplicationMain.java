edbd79073d328fd65c81ba840a99e3177ad9001b
Jeppe Andersen <nocture@nocture.dk>
2012-09-25 12:06:48 +0200

    Watchdog project

---
+++ Code/Watchdog/src/system/SCJApplicationMain.java
@@ -0,0 +1,11 @@
+package system;
+
+import javax.safetycritical.JopSystem;
+
+public class SCJApplicationMain
+{
+	public static void main(String[] args)
+	{
+		//JopSystem.startMission(new BlinkerSafelet());
+	}
+}
