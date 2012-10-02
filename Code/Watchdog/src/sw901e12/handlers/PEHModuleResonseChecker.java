6f66343e4a971eb0fed6fe2e8eb6e24f191dfe41
Todberg <mtodbe08@student.aau.dk>
2012-10-02 15:45:51 +0200

    no message

---
+++ Code/Watchdog/src/sw901e12/handlers/PEHModuleResonseChecker.java
@@ -0,0 +1,43 @@
+package sw901e12.handlers;
+
+import javax.realtime.PeriodicParameters;
+import javax.realtime.PriorityParameters;
+import javax.safetycritical.PeriodicEventHandler;
+import javax.safetycritical.StorageParameters;
+import javax.safetycritical.annotate.Level;
+import javax.safetycritical.annotate.SCJAllowed;
+import javax.safetycritical.io.SimplePrintStream;
+
+import sw901e12.Module;
+import sw901e12.comm.ModulePinger;
+
+public class PEHModuleResonseChecker extends PeriodicEventHandler {
+
+	private Module[] slaves;
+	private SimplePrintStream console;
+	
+	public PEHModuleResonseChecker(PriorityParameters priority,
+			PeriodicParameters parameters, StorageParameters scp, long scopeSize, Module[] slaves, SimplePrintStream console) {
+		super(priority, parameters, scp, scopeSize);
+		
+		this.slaves = slaves;
+		this.console = console;
+	}
+
+	@Override
+	@SCJAllowed(Level.SUPPORT)
+	public void handleAsyncEvent() {
+		console.println("Checking module responses");
+		ModulePinger slaveModulePinger;
+		
+		for(Module slave : slaves) {
+			slaveModulePinger = slave.getModulePinger();
+			if(slaveModulePinger.DidReceiveResponseFromModule()) {
+				slaveModulePinger.ResetDidReceiveResponseFlag();
+			} else {
+				
+			}
+			
+		}
+	}
+}
