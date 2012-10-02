8be4fcbd675b90495282e891ed3a1641b6101087
Jeppe Andersen <nocture@nocture.dk>
2012-10-02 13:42:46 +0200

    Updates to I2C comm

--- Code/Watchdog/src/sw901e12/comm/ModulePingerFactory.java
+++ Code/Watchdog/src/sw901e12/comm/ModulePingerFactory.java
@@ -8,7 +8,7 @@ import com.jopdesign.sys.Native;
  * Therefore each module will have its own pinger that knows how to communicate with that particular device
  * All pinger objects are created through a factory that determines the correct pinger for a given address on the bus
  * An Abstract Factory is implemented here to provide a factory on run time depending on whether the application runs
- * in the simulator or on actual hardware.
+ * in the simulator or on actual hardware. 
  */
 public abstract class ModulePingerFactory {
 	
