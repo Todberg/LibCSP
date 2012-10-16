4ff0cf5af076ef5a616d5522ba7a11bc5c0f8d2c
Todberg <mtodbe08@student.aau.dk>
2012-10-16 11:34:28 +0200

    Put  loop bounds on RecoveryOptionOne

--- Code/Watchdog/src/sw901e12/RecoveryOptionOne.java
+++ Code/Watchdog/src/sw901e12/RecoveryOptionOne.java
@@ -20,10 +20,10 @@ public class RecoveryOptionOne implements IRecoveryRoutine {
 		
 		// Simulate recovery computations
 		ledSwitch.ledSwitch = Integer.MAX_VALUE;
-		for(int i = 0; i < 10000000; i++);
+		for(int i = 0; i < 1000; i++); // @WCA loop=1000
 		ledSwitch.ledSwitch = 0;
 		sysDevice.wd = Integer.MAX_VALUE;
-		for(int i = 0; i < 10000000; i++);
+		for(int i = 0; i < 1000; i++); // @WCA loop=1000
 		sysDevice.wd = 0;
 	}
 }
