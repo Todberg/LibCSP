e704d66b031168df0ac1921933d69e75bf7f7de5
Todberg <mtodbe08@student.aau.dk>
2012-10-29 14:33:43 +0100

    no message

---
+++ Code/LibCSP/src/system/SCJApplication.java
@@ -0,0 +1,12 @@
+package system;
+
+import javax.safetycritical.JopSystem;
+
+public class SCJApplication {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("main");
+	
+	}
+}
