4ab8fed80a00e5973e2d1b6b7247bde6f18264e0
Todberg <mtodbe08@student.aau.dk>
2012-12-04 15:51:19 +0100

    tekst

---
+++ Report/Code/handler.java
@@ -0,0 +1,8 @@
+alignmentHandler = new PEHAlignmentHandler(
+  new PriorityParameters(10);,
+  new PeriodicParameters(new RelativeTime(0,0),new RelativeTime(500,0)), 
+  new StorageParameters(1000,new long[] {500},0,0);, 
+  0);
+ 
+ alignmentHandler.register();	
+
