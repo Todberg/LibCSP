569314d251b6b347bb19c1d7c3494b7e48e5c9aa
Jeppe Andersen <nocture@nocture.dk>
2012-12-15 19:04:19 +0100

    Reflection

--- Report/Code/Implementation/connectiondispose.java
+++ Report/Code/Implementation/connectiondispose.java
@@ -2,6 +2,6 @@
 public void dispose() {
 	this.isOpen = false;
 	this.id = 0;
-	this.packets.reset();
+	this.packets.reset(); // Calls dispose on each packet
 	CSPManager.resourcePool.putConnection(this);
 }
--- Report/Code/Implementation/interfacei2c.java
+++ Report/Code/Implementation/interfacei2c.java
@@ -1,18 +1,21 @@
 public class InterfaceI2C implements IMACProtocol {
 	@Override
 	public void initialize(int MACAddress) {	
+		// JOP specific factory to get HW objects
 		I2CPort = I2CFactory.getFactory().getI2CportA();
 		I2CPort.initialize(MACAddress, true);
 	}
 
 	@Override
 	public void transmitPacket(PacketCore packet) {
-		..
+		// Make frame and send using HW object
+		...
 		CSPManager.resourcePool.putPacket(packet);
 	}
 	@Override
 	public void receiveFrame() {		
 		PacketCore packet = CSPManager.resourcePool.getPacket(CSPManager.TIMEOUT_SINGLE_ATTEMPT);
+		// Get frame using HW object and extract packet
 		..
 		RouteHandler.packetsToBeProcessed.enqueue(packet);		
 	}
