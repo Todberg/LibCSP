d38709165cfdb94aa79fde257ecaf607d751afac
Todberg <mtodbe08@student.aau.dk>
2012-12-15 09:19:10 +0100

    asd

--- Code/LibCSP/src/sw901e12/csp/testapplication/ClientServerMission.java
+++ Code/LibCSP/src/sw901e12/csp/testapplication/ClientServerMission.java
@@ -44,11 +44,16 @@ public class ClientServerMission extends Mission implements Safelet<Mission> {
 		
 		manager.init((byte)ClientServerMission.NODE_ADDRESS);
 		manager.initPools();
-		manager.startRouteHandler();
 		
 		initializeFirstClientHandler();
+		manager.routeSet(ClientServerMission.NODE_ADDRESS, manager.getIMACProtocol(manager.INTERFACE_LOOPBACK), 0x71);
+		
 		initializeSecondClientHandler();
+		manager.routeSet(ClientServerMission.NODE_ADDRESS, manager.getIMACProtocol(manager.INTERFACE_LOOPBACK), 0x3C);
+		
 		initializeServerHandler();
+		
+		manager.startRouteHandler();
 	}
 	
 	
--- Report/Code/Implementation/mission-init.java
+++ Report/Code/Implementation/mission-init.java
@@ -7,13 +7,18 @@ public class ClientServerMission extends Mission implements Safelet<Mission> {
   @SCJAllowed(Level.SUPPORT)
   protected void initialize() {
     manager = new CSPManager();
+
     manager.init((byte)ClientServerMission.NODE_ADDRESS);
     manager.initPools();
-    manager.startRouteHandler();
-
+    
     initializeFirstClientHandler();
+    manager.routeSet(ClientServerMission.NODE_ADDRESS, manager.getIMACProtocol(manager.INTERFACE_LOOPBACK), 0x71);
     initializeSecondClientHandler();
+    
+    manager.routeSet(ClientServerMission.NODE_ADDRESS, manager.getIMACProtocol(manager.INTERFACE_LOOPBACK), 0x3C);
     initializeServerHandler();
+
+    manager.startRouteHandler();
   }
   ...
 }
